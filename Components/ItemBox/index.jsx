49428c8810ea72be1f57bde6d74cdd44b52ab552
Miss <melissarios497@gmail.com>
2021-07-16 17:18:09 -0300

    add PropTypes

--- Components/ItemBox/index.jsx
+++ Components/ItemBox/index.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 
+import PropTypes from "prop-types";
+
 import { classnames } from "tailwindcss-classnames";
 
-function ItemBox({ onClick, text, onChange, checked, id, handleEditItem }) {
+function ItemBox({ text, onChange, checked, id, handleEditItem, onClick }) {
   const [edit, setEdit] = useState(false);
   const [value, setValue] = useState(text);
 
@@ -63,17 +65,16 @@ function ItemBox({ onClick, text, onChange, checked, id, handleEditItem }) {
 
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w- m-2"
+          className="h-6 w-6"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
           onClick={onClick}
         >
           <path
-            onClick={onClick}
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
             d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
           />
         </svg>
@@ -90,5 +91,13 @@ function ItemBox({ onClick, text, onChange, checked, id, handleEditItem }) {
     </div>
   );
 }
+ItemBox.PropTypes = {
+  text: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  checked: PropTypes.bool,
+  id: PropTypes.number.isRequired,
+  handleEditItem: PropTypes.func,
+  onClick: PropTypes.func.isRequired,
+};
 
 export default ItemBox;
